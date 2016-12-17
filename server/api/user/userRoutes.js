b519b7ab0f0878f340f93c94fc0b5e4088daae76
dustinphou <ubuntu@ip-172-31-8-39.us-west-2.compute.internal>
2016-12-17 07:00:59 +0000

    Added get,post,delete for user by id.

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -7,7 +7,7 @@ var user = require('./userModel.js');
 //So if you have same URL but with different HTTP OP such as POST,GET etc
 //Then use route() to remove redundant code.
 router.route('/')
-  .get(function(req, res){    
+  .get(function(req, res, next){    
     user.find(function (err, users) {
  		if (err) next();
    		res.send(users);
@@ -17,30 +17,34 @@ router.route('/')
   if(!req.body.username || !req.body.address) next();
   var newuser = new user(req.body);
   newuser.save(function (err, user) {
-  	if(err) 
-      {
-        console.log("??????????????"); 
-        return next(err);
-      }
+  	if(err) return next(err);
   	});
   res.send();  
   });
 
-// router.route('/:user_id')
-// 	.get(function(req, res){    
-//     user.find({_id: req._id}, function (err, user) {
-//  		if (err) return console.error(err);
-//    		res.send(user);
-//   });
-// })
-//   .post(function(req, res){
-//   if(!req.body.username || !req.body.address) next();
-//   var newuser = new user(req.body);
-//   newuser.save(function (err, user) {
-//   	if(err) next(err);
-//   	});
-//   res.send();  
-//   });
+router.route('/:user_id')
+	.get(function(req, res, next){    
+    user.find({_id: req.params.user_id}, function (err, user) {
+ 		if (err) next();
+      res.send(user);
+  });
+})
+  .put(function(req, res, next){
+    console.log(req.body);
+  if(!req.body.username || !req.body.address) next();
+  user.update({_id: req.params.user_id}, {$set: {username: req.body.username, address: req.body.address}}, function (err, user) {
+  	if(err) next(err);
+  	});
+  res.send();  
+  })
+  .delete(function(req, res, next){    
+    user.find({_id: req.params.user_id}, function (err, user) {
+            if (err) next()})
+        .remove(function (err, user) {
+            if (err) next();
+            });
+      res.send("Deleted");
+  });
 
 module.exports = router;
 
